365fe96b2539f2a691422a5a229168250f77f689
labordus <bordumar@gmail.com>
2020-07-02 18:48:58 +0200

    Second Commit

--- app/src/main/java/com/example/pil/MyBroadcastReceiver.kt
+++ app/src/main/java/com/example/pil/MyBroadcastReceiver.kt
@@ -1,6 +1,7 @@
 package com.example.pil
 
 
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -13,5 +14,6 @@ class MyBroadcastReceiver: BroadcastReceiver(){
 //        Toast.makeText(context,extra, Toast.LENGTH_LONG).show()
 //        Toast.makeText(applicationContext,"this is toast message",Toast.LENGTH_SHORT).show()
         Toast.makeText(context,"this is toast message",Toast.LENGTH_LONG).show()
+//        TestIt("Heel erg grappig")
     }
 }
---
+++ app/src/main/java/com/example/pil/NotificationUtils.kt
@@ -0,0 +1,17 @@
+package com.example.pil
+
+import android.app.Activity
+import android.app.Application
+import android.app.NotificationManager
+import android.app.PendingIntent.getActivity
+import android.content.Context
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.pil.MainActivity
+import com.example.pil.R
+
+
+//fun TestIt(messageBody: String) {
+//    Toast.makeText(com.example.pil.MainActivity(), messageBody, Toast.LENGTH_LONG).show()
+//}
